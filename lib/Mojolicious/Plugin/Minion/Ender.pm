f8e18605a9d7d05ea44d722baa0a6e7bdb2284e4
Simone Cesano <simonecesano@libero.it>
2021-01-17 03:31:10 +0100

    Stable; cleaned up; no support for daemonized server

--- lib/Mojolicious/Plugin/Minion/Ender.pm
+++ lib/Mojolicious/Plugin/Minion/Ender.pm
@@ -5,6 +5,7 @@ use Mojo::File qw/path/;
 use Mojo::Util qw/dumper/;
 
 use Config;
+use Try::Tiny;
 
 my $app;
 
@@ -43,32 +44,18 @@ sub before_server_start_hook{
 	# Mojo::Server::PSGI + starman: parent is starman
 	# Mojo::Server::Daemon morbo: parent is not shell
 
-	# local $SIG{TERM} = sub { print STDERR "got TERM\n" };
-
 	my $msg_done;
-	unless (grep { ref $server eq $_ } @servers_ok) {
+	if (ref $server eq 'Mojo::Server::Daemon') {
+	} elsif (ref $server eq 'Mojo::Server::Prefork') {
+	    $app->log->info(sprintf "Warning: %s does not support daemonized server type %s", __PACKAGE__, ref $server) unless $msg_done++;
+	} else {
 	    $app->log->info(sprintf "%s does not support server type %s", __PACKAGE__, ref $server) unless $msg_done++;
 	    return;
 	}
 
-	# local $SIG{HUP} = sub {
-	#     $app->log->info("got HUP in $$ " . getppid());
-	#     # exit 0
-	# };
-
 	$self->root_process($$);
 	$self->spawn_worker for (0..($spawn - 1));
     }
-}
-
-sub catch_sig {
-    my $self = shift;
-    # defined $Config{sig_name} or die "No sigs?";
-    # $self->app->log->info($Config{sig_name});
-
-    # foreach my $name (grep { !/ZERO/ } split(' ', $Config{sig_name})) {
-    # 	local $SIG{$name} = sub { $self->app->log->info("caught $name in $$") };
-    # }
 }
 
 sub spawn_worker {
@@ -76,6 +63,7 @@ sub spawn_worker {
     if (my $pid = fork) {
 	push @{$self->workers}, $pid;
 	push @workers, $pid;
+	return;
     } else {
 	$app->log->info("Starting minion worker $$ inside " . getppid());
 	$app->minion->worker->run;
